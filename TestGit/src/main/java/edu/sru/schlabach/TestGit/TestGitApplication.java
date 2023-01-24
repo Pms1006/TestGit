4bd20bc7b89ddc3395c9da5fd6f79c8a0f51f147
pms1006 <pms1006@AT224XWKS-007W.srunet.sruad.edu>
2023-01-24 10:07:45 -0500

    My test project

---
+++ TestGit/src/main/java/edu/sru/schlabach/TestGit/TestGitApplication.java
@@ -0,0 +1,14 @@
+package edu.sru.schlabach.TestGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ TestGit/src/test/java/edu/sru/schlabach/TestGit/TestGitApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sru.schlabach.TestGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
